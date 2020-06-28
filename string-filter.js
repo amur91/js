63eed0b812c8bb6e7511ff62b26f404b2de0b684
amur91 <58135032+amur91@users.noreply.github.com>
2020-06-28 17:37:18 +0300

    Add files via upload

---
+++ string-filter.js
@@ -0,0 +1,16 @@
+const string = "Привет! Как дела?";
+const vowels = ["у","е","ы","а","о","э","я","и","ю","ё"]
+
+const getVowels = stringToFilter => {
+    let extractedVowels = "";
+
+    for (let i=0; i < stringToFilter.length; i++) {
+        const currentLetter = stringToFilter[i].toLowerCase();
+
+        if (vowels.includes(currentLetter)) {
+           extractedVowels = extractedVowels + currentLetter;
+        }
+    }
+return extractedVowels;
+}
+console.log(getVowels(string));
